1e994a2a0ea7fab9f8244343b135e732767067f7
Trevor Scherling & Justin Scott <student@epicodus.com>
2015-10-14 14:07:10 -0700

    Finished CRUD functionality for Genre class

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -29,6 +29,19 @@ class GenresController < ApplicationController
     redirect_to genres_path
   end
 
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to genres_path
+    else
+      render :edit
+    end
+  end
+
 private
   def genre_params
     params.require(:genre).permit(:name)
